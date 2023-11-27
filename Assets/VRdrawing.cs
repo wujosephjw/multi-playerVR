8da1e5e493bf5f1128a576e4a98757a79b38b7b3
joseph90027 <joseph90027@gmail.com>
2023-11-27 04:25:55 -0500

    Change to XR Action-based system

--- Assets/VRdrawing.cs
+++ Assets/VRdrawing.cs
@@ -6,48 +6,54 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class VRdrawing : MonoBehaviour
 {
-    public XRController drawingController;
-    public GameObject drawingPrefab; // The prefab of the line or trail renderer
-
-    private GameObject currentDrawing;
-
+    public GameObject meshPrefab; // The prefab for the mesh you want to draw
+    //public XRController xrController;
+    public InputHelpers.Button drawButton;
+    public float activationThreshold = 0.1f;
     private bool isDrawing = false;
+    public XRController leftController;
+    public XRController rightController;
 
-    private void Update()
+
+    void Start()
     {
-        if (drawingController)
+        leftController = GetComponent<XRController>();
+    }
+
+    void Update()
+    {
+        CheckForDrawingInput();
+    }
+
+    void CheckForDrawingInput()
+    {
+        // Check if the trigger button is pressed
+        if (CheckIfButtonDown(leftController))
         {
-            CheckForDrawingInput();
+            StartDrawing();  
+        }
+        else
+        {
+            StopDrawing();
         }
     }
 
-    private void CheckForDrawingInput()
+    public bool CheckIfButtonDown(XRController controller)
     {
-        // Check if the drawing button is pressed
-        if (drawingController.inputDevice.TryGetFeatureValue(CommonUsages.triggerButton, out bool isDrawingButtonPressed))
-        {
-            if (isDrawingButtonPressed && !isDrawing)
-            {
-                StartDrawing();
-            }
-            else if (!isDrawingButtonPressed && isDrawing)
-            {
-                StopDrawing();
-            }
-        }
+        InputHelpers.IsPressed(controller.inputDevice, drawButton, out bool isPressed, activationThreshold);
+        return isPressed;
     }
 
-    private void StartDrawing()
+    void StartDrawing()
     {
         isDrawing = true;
-
-        // Instantiate the drawing prefab
-        currentDrawing = Instantiate(drawingPrefab, transform.position, transform.rotation, transform);
+        // Instantiate the mesh prefab or create your custom mesh generation logic
+        // You might want to use XRGrabInteractable for better interaction control
+        Instantiate(meshPrefab, leftController.transform.position, leftController.transform.rotation);
     }
 
-    private void StopDrawing()
+    void StopDrawing()
     {
         isDrawing = false;
-        currentDrawing = null;
     }
 }
