e1e4392862b36a391322f15191ea2c87a72fdd05
joseph90027 <joseph90027@gmail.com>
2023-11-29 06:23:16 -0500

    FInalize mesh creation process script

--- Assets/MeshCreation.cs
+++ Assets/MeshCreation.cs
@@ -9,44 +9,34 @@ public class MeshCreation : MonoBehaviour
 
     private List<Vector3> positionsList = new List<Vector3>();
     private Mesh mesh;
+    private List<int> triangles = new List<int>();
+
+    private int numOfLeftTrigger = 0;
+    private int firstClosestPointIndex = -1;
+    private int secondClosestPointIndex = -1;
 
     void Start()
     {
-        InitializeMesh();
-    }
-
-    void InitializeMesh()
-    {
+        // Initialize mesh
         mesh = new Mesh();
         meshFilter.mesh = mesh;
-    }
-
-    void UpdateMesh()
-    {
-        mesh.vertices = positionsList.ToArray();
-        TriangulateMesh();
-    }
-
-    void TriangulateMesh()
-    {
-        // Triangulate the points using Unity's built-in function
-        mesh.triangles = Triangulate(positionsList.ToArray());
-        mesh.RecalculateNormals();
     }
 
     public void LeftTriggerClick()
     {
         if (leftController != null)
         {
-            Vector3 controllerPosition = leftController.transform.position;
-            positionsList.Add(controllerPosition);
+            numOfLeftTrigger++;
 
-            if (spherePrefab != null)
+            Vector3 controllerPosition = leftController.transform.position;
+
+            if (numOfLeftTrigger % 3 == 0 || numOfLeftTrigger < 3)
             {
-                //Instantiate(spherePrefab, controllerPosition, Quaternion.identity);
+                AddPositionAndInstantiateSphere(controllerPosition);
             }
 
             UpdateMesh();
+
         }
         else
         {
@@ -54,25 +44,109 @@ public class MeshCreation : MonoBehaviour
         }
     }
 
-    // Triangulation using Unity's built-in method
+    void UpdateMesh()
+    {
+        mesh.vertices = positionsList.ToArray();
+        mesh.triangles = Triangulate(positionsList.ToArray());
+        mesh.RecalculateNormals();
+    }
+
+    void AddPositionAndInstantiateSphere(Vector3 position)
+    {
+        positionsList.Add(position);
+        Instantiate(spherePrefab, position, Quaternion.identity);
+    }
+
     int[] Triangulate(Vector3[] points)
     {
-        List<int> indices = new List<int>();
+        Vector3 controllerPosition = leftController.transform.position;
         int pointCount = points.Length;
 
-        // Triangulate the points
-        for (int i = 0; i < pointCount - 2; i++)
+        if (numOfLeftTrigger <= 3)
         {
-            indices.Add(i);
-            indices.Add(i + 1);
-            indices.Add(i + 2);
-
-            // Choose any two vertices of the current face to be the same as any two vertices of the previous face
-            indices.Add(i);
-            indices.Add(i + 2);
-            indices.Add(i + 1);
+            return UnityTriangulate(points);
+        }
+        else if (numOfLeftTrigger % 3 != 0)
+        {
+            UpdateClosestPointIndices(points, controllerPosition);
+        }
+        else if (numOfLeftTrigger % 3 == 0 && firstClosestPointIndex != -1 && secondClosestPointIndex != -1)
+        {
+            return CreateTrianglesWithClosestPoints(pointCount);
         }
 
-        return indices.ToArray();
+        return triangles.ToArray();
+    }
+
+    
+    int[] UnityTriangulate(Vector3[] points)
+    {
+        //List<int> triangles = new List<int>();
+        for (int i = 0; i < points.Length - 2; i++)
+        {
+            triangles.Add(i);
+            triangles.Add(i + 1);
+            triangles.Add(i + 2);
+
+            triangles.Add(i);
+            triangles.Add(i + 2);
+            triangles.Add(i + 1);
+        }
+        return triangles.ToArray();
+    }
+
+    void UpdateClosestPointIndices(Vector3[] points, Vector3 targetPoint)
+    {
+        if (firstClosestPointIndex == -1 && secondClosestPointIndex == -1)
+        {
+            firstClosestPointIndex = FindClosestPointIndex(points, targetPoint);
+        }
+        else if (firstClosestPointIndex != -1 && secondClosestPointIndex == -1)
+        {
+            secondClosestPointIndex = FindClosestPointIndex(points, targetPoint);
+        }
+    }
+
+    int[] CreateTrianglesWithClosestPoints(int pointCount)
+    {
+        //List<int> triangles = new List<int>();
+
+        triangles.Add(firstClosestPointIndex);
+        triangles.Add(secondClosestPointIndex);
+        triangles.Add(pointCount - 1);
+
+        triangles.Add(firstClosestPointIndex);
+        triangles.Add(pointCount - 1);
+        triangles.Add(secondClosestPointIndex);
+
+        firstClosestPointIndex = -1;
+        secondClosestPointIndex = -1;
+
+        return triangles.ToArray();
+    }
+
+    int FindClosestPointIndex(Vector3[] points, Vector3 targetPoint)
+    {
+        if (points == null || points.Length == 0)
+        {
+            Debug.LogError("Array of points is null or empty.");
+            return -1;
+        }
+
+        int closestPointIndex = 0;
+        float minDistance = Vector3.Distance(targetPoint, points[0]);
+
+        for (int i = 1; i < points.Length; i++)
+        {
+            float distance = Vector3.Distance(targetPoint, points[i]);
+
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                closestPointIndex = i;
+            }
+        }
+
+        return closestPointIndex;
     }
 }
---
+++ Assets/MeshCreation2.cs
@@ -0,0 +1,78 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshCreation2 : MonoBehaviour
+{
+    public GameObject leftController;
+    public GameObject spherePrefab;
+    public MeshFilter meshFilter;
+
+    private List<Vector3> positionsList = new List<Vector3>();
+    private Mesh mesh;
+
+    void Start()
+    {
+        InitializeMesh();
+    }
+
+    void InitializeMesh()
+    {
+        mesh = new Mesh();
+        meshFilter.mesh = mesh;
+    }
+
+    void UpdateMesh()
+    {
+        mesh.vertices = positionsList.ToArray();
+        TriangulateMesh();
+    }
+
+    void TriangulateMesh()
+    {
+        // Triangulate the points using Unity's built-in function
+        mesh.triangles = Triangulate(positionsList.ToArray());
+        mesh.RecalculateNormals();
+    }
+
+    public void LeftTriggerClick()
+    {
+        if (leftController != null)
+        {
+            Vector3 controllerPosition = leftController.transform.position;
+            positionsList.Add(controllerPosition);
+
+            if (spherePrefab != null)
+            {
+                //Instantiate(spherePrefab, controllerPosition, Quaternion.identity);
+            }
+
+            UpdateMesh();
+        }
+        else
+        {
+            Debug.LogError("VR Controller is not assigned!");
+        }
+    }
+
+    // Triangulation using Unity's built-in method
+    int[] Triangulate(Vector3[] points)
+    {
+        List<int> indices = new List<int>();
+        int pointCount = points.Length;
+
+        // Triangulate the points
+        for (int i = 0; i < pointCount - 2; i++)
+        {
+            indices.Add(i);
+            indices.Add(i + 1);
+            indices.Add(i + 2);
+
+            // Choose any two vertices of the current face to be the same as any two vertices of the previous face
+            indices.Add(i);
+            indices.Add(i + 2);
+            indices.Add(i + 1);
+        }
+
+        return indices.ToArray();
+    }
+}
